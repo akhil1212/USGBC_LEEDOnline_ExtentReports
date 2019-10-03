28bbcaddeb1d019d66f008dd68f3594bec0ba9fe
SurishettySainath <ssainath2172@gmail.com>
2019-10-03 16:17:37 +0530

    get path

--- USGBC_LEED/Keywords/projectKeywords/LeedOnlineKeywords.groovy
+++ USGBC_LEED/Keywords/projectKeywords/LeedOnlineKeywords.groovy
@@ -1754,7 +1754,7 @@ class LeedOnlineKeywords {
 		File src =screen.getScreenshotAs(OutputType.FILE);
 		File target = new File(dest);
 		FileUtils.copyFile(src,target);
-		return target.getName()
+		return "screenshots/"+target.getName()
 	}
 
 	//	@Keyword
