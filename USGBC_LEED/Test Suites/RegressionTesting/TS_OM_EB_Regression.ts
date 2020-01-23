e9dba95e4e257c30f9b94f56f263007af5a59e67
akhil1212 <akhilgelli1141@gmail.com>
2020-01-23 18:03:42 +0530

    Running regression Test

--- USGBC_LEED/Test Suites/RegressionTesting/TS_OM_EB_Regression.ts
+++ USGBC_LEED/Test Suites/RegressionTesting/TS_OM_EB_Regression.ts
@@ -13,19 +13,19 @@
    <testCaseLink>
       <guid>795e52fa-a68b-499a-af4c-d61108447e77</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/OM_ExistingBuildings/TC_GreenCleaning</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>a2ff611a-611c-49b0-947d-37c58806a18c</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/OM_ExistingBuildings/TC_SiteManagement</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>009ffedf-a4d3-4ffd-bcd3-2d7f03094378</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/OM_ExistingBuildings/TC_GridHarmonization</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -43,7 +43,7 @@
    <testCaseLink>
       <guid>92a0975b-9e8b-4f08-aa53-8f5f865fc189</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/OM_ExistingBuildings/TC_Innovation</testCaseId>
    </testCaseLink>
    <testCaseLink>
